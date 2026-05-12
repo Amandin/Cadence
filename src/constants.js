b3668dc2b0bf8141a63255f93e26a5e832ffeed0
Anadethio <amandint@gmail.com>
2026-05-13 00:01:15 +0200

    Bump version for eye polish

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const APP_VERSION = '0.1.11';
+export const APP_VERSION = '0.1.12';
 export const STORAGE_KEY = 'cadence:campaign:v1';
 
 export const participantKinds = ['PJ', 'Allié', 'Opposition', 'Horloge', 'Autre'];
