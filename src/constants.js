bcd0e3e698e40b03017cab47d504bb182be91b3b
Anadethio <amandint@gmail.com>
2026-05-13 21:14:59 +0200

    Bump version for dark bottom bar contrast fix

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const APP_VERSION = '0.1.26';
+export const APP_VERSION = '0.1.27';
 export const STORAGE_KEY = 'cadence:campaign:v1';
 export const TEMPLATE_STORAGE_KEY = 'cadence:templates:v1';
 
