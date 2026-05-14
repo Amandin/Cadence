858c6b5cb6536bd64d5e630390c75826625a399c
Anadethio <amandint@gmail.com>
2026-05-14 23:00:39 +0200

    Bump version for campaign naming

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const APP_VERSION = '0.1.75';
+export const APP_VERSION = '0.1.76';
 export const STORAGE_KEY = 'cadence:campaign:v1';
 export const TEMPLATE_STORAGE_KEY = 'cadence:templates:v1';
 
