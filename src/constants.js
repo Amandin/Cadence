911b39f9bb4603e8227c4ca6af841a1e176d9bcc
Anadethio <amandint@gmail.com>
2026-05-14 09:58:29 +0200

    Bump version for tiebreaker demo cases

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const APP_VERSION = '0.1.43';
+export const APP_VERSION = '0.1.44';
 export const STORAGE_KEY = 'cadence:campaign:v1';
 export const TEMPLATE_STORAGE_KEY = 'cadence:templates:v1';
 
