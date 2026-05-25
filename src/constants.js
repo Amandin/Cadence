e7ed2c9ed9e4a55b0f30944f161b9cce5444acb1
Amandin <amandint@gmail.com>
2026-05-25 13:20:57 +0200

    Release Cadence 0.3.0

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const APP_VERSION = '0.2.11.1';
+export const APP_VERSION = '0.3.0';
 export const STORAGE_KEY = 'cadence:campaign:v1';
 export const TEMPLATE_STORAGE_KEY = 'cadence:templates:v1';
 
